1c3d981703c7a63a84c2094dda4e849528782a43
nemeab <94843349+nemeab@users.noreply.github.com>
2023-02-08 22:58:02 -0600

    git commit -m "add cart button"

--- food_delivery/lib/Pages/HomePage.dart
+++ food_delivery/lib/Pages/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../Widgets/AppBarWidget.dart';
 import '../Widgets/CategoriesWidget.dart';
@@ -95,8 +96,6 @@ class HomePage extends StatelessWidget {
           //Popular Items Widget
           PopularItemsWidget(),
 
-
-
           //Newest Items
           Padding(
             padding: EdgeInsets.only(top: 20, left: 10),
@@ -112,6 +111,27 @@ class HomePage extends StatelessWidget {
           //Newest Item Widget
           NewestItemsWidget(),
         ],
+      ),
+      //drawer:  ,
+      floatingActionButton: Container(
+        decoration:
+            BoxDecoration(borderRadius: BorderRadius.circular(20), boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 10,
+            offset: Offset(0, 3),
+          ),
+        ]),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(
+            CupertinoIcons.cart,
+            size: 28,
+            color: Colors.red,
+          ),
+          backgroundColor: Colors.white,
+        ),
       ),
     );
   }
